512798897a70b3611357254c35d45275d81c0bc6
Leandro Fontellas Laurito <leandrofontellas@gmail.com>
2021-01-28 17:47:40 -0300

    aulas de recuperação de senha

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,15 +2,16 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
-
-const Button: React.FC<ButtonProps> = ({ children, ...restProps }) => {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => {
   // apesar da gente passar o type dps o eslint n gosta que a gente n passa
   // o type pro button entao a gente vai passar um type generico e
   // dps ele vai ser sobrescrito caso necessario pela props.
   return (
-    <Container type="button" {...restProps}>
-      {children}
+    <Container type="button" {...rest}>
+      {loading ? 'Carregando...' : children}
     </Container>
   );
 };
